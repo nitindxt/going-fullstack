9f8fd7ba8dd3c4ad79c25463b6fb88f85ad6f0d2
Nitin Dixit <nitinajaydixit@gmail.com>
2022-06-17 05:37:19 +0530

    add custom header

--- app.mjs
+++ app.mjs
@@ -1,11 +1,16 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req,res)=>{
-        res.send('hello there!')
-})
+app.get("/", (req, res) => {
+  res.send("hello there!");
+});
 
-app.listen(port, ()=>
-        console.log(`server is listening at ${port}`))
+app.listen(port, () => console.log(`server is listening at ${port}`));
+
+app.get("/demo", (req, res) => {
+  res.set("X-full-stack", "4life"); //custom header
+  res.status(418);
+  res.send("I prefer coffee");
+});
